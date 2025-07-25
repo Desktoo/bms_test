93282b97e2530c70a6ea3d38acefc984e66c0cb5
Desktoo <119888989+Desktoo@users.noreply.github.com>
2025-07-25 22:39:32 +0530

    Update index.ts

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json())
 
 app.get("/", (req, res) => {
-  res.send("Hi there")
+  res.send("Hi there, now this change is directly done on the github")
 })
 
 app.post("/signup", async (req, res) => {
